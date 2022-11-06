59405a8f6890ec9703618fa64752c516cd0ca380
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2022-11-06 20:50:05 +0500

    feat:add is link

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const btn_open = document.getElementById("btn_open");
+const register = document.getElementById("register");
+const primary_block = document.getElementById("primary-block");
+open.onclick = function () {
+    register.style.display = "block";
+    primary_block.display.style.display = "none";
+};
