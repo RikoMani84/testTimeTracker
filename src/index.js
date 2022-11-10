f0853a26a2dd42c21baed2606f933b19eb0b8a46
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2022-11-10 11:03:24 +0500

    fix: change code in js file to fix bug

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const primary_block = document.getElementById("primary-block");
 const secondary_block = document.getElementById("secondary-block");
 const login = document.getElementById("login");
 const enter = document.getElementById("enter");
+const _name = document.getElementById("_name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const btn_lg = document.getElementById("btn_lg");
@@ -28,12 +29,11 @@ function open_login_block() {
     primary_block.style.display = "none";
 };
 
-
-
 function addEmployees() {
     fetch('http://127.0.0.1:3000/employees', {
         method: 'POST',
         body: JSON.stringify({
+            name: _name.value,
             email: email.value,
             password: password.value,
         }),
@@ -43,5 +43,24 @@ function addEmployees() {
     })
         .then((response) => response.json())
         .then((json) => console.log(json));
-        
 }
+
+function sceondryBlock() {
+    var xhttp = new XMLHttpRequest;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            var employees = response;
+            console.log(employees);
+            var item = '';
+            for (var i = 0; i < employees.length; i++) {
+                item += `Hello, <span id="name"> ${employees[i].name} </span>`
+            }
+            document.getElementById('titleSC').innerHTML = item;
+        }
+    };
+
+    xhttp.open("GET", "http://localhost:3000/employees", true);
+    xhttp.send();
+}
+sceondryBlock();    
